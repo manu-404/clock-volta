a9901f3f1c13452ddd8f48c98ea5c655e33d4671
Manuel Bonament <manuel.bonament@outlook.fr>
2024-02-22 13:56:55 +0100

    Initialization ReactJS and MUI

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import Clock from './components/Clock/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+    </div>
+  );
+}
+
+export default App;
